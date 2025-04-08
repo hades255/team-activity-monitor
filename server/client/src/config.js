ee101b79c2eefbe7d1887af665eef3d48fa40833
hades255 <hades.255@outlook.com>
2025-04-08 19:41:22 +0900

    change server url

--- server/client/src/config.js
+++ server/client/src/config.js
@@ -1,1 +1,2 @@
-export const SERVER_API_PATH = process.env.REACT_APP_API_URL || 'http://localhost:3000/api'; 
+export const SERVER_API_PATH = process.env.REACT_APP_API_URL || 'http://167.88.39.55:3100/api'; 
+// export const SERVER_API_PATH = process.env.REACT_APP_API_URL || 'http://localhost:3000/api'; 
