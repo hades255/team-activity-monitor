9c5efaf5ff054fe21e3a390815dbaa133ecb0ac9
S.M <139430741+hades255@users.noreply.github.com>
2025-10-23 22:11:08 +0900

    add banned apps

--- server/contants/index.js
+++ server/contants/index.js
@@ -9,6 +9,8 @@ const BANNED_APPS = [
   "game.exe",
   "fifa.exe",
   "call_to_arms.exe",
+  "vietnam.exe",
+  "mowas_2.exe"
 ];
 
 const BANNED_APPS_TITLE = {
@@ -19,7 +21,9 @@ const BANNED_APPS_TITLE = {
   "war3.exe": "Dota 1",
   "game.exe": "RA 2",
   "fifa.exe": "FIFA",
-  "call_to_arms.exe": "MOW 2",
+  "call_to_arms.exe": "Call to ARMs",
+  "vietnam.exe": "MOW Vietnam",
+  "mowas_2.exe": "MOW 2"
 };
 
 module.exports = {
